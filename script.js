be95061b7a795c00572f7acb3d915bc12d54373f
CamzJJ <cameronlaudat@hotmail.com>
2023-03-29 13:08:38 +0100

    implementing dice roll

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+
+const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
+
+// starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEL.classList.add('hidden');
+
+// Rolling dice funtionality
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  diceEL.classList.remove('hidden');
+  diceEL.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    currentScore = 0;
+  }
+});
